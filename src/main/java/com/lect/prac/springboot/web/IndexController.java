5fc3a514341cc1ec012335ca96332a25b8ab97ed
lidbera <lidbera@naver.com>
2020-05-29 01:38:57 +0900

    oauth-4

--- src/main/java/com/lect/prac/springboot/web/IndexController.java
+++ src/main/java/com/lect/prac/springboot/web/IndexController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
     private final PostsService postsService;
 
-    @GetMapping("/")
+    @GetMapping("/")    
     public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
 
